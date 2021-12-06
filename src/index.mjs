4cb49f4272b0c30a7028b8429ddcedca2b6eb960
Levi Wolfe <levi.wolfe@gmail.com>
2021-12-06 11:07:15 -0800

    Enable logging

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,14 @@
 import { track } from 'workers-logger'
+import { enable } from 'diary'
 
 const reporter = events => void console.log(events)
 
 // JS version of https://github.com/maraisr/workers-logger/blob/main/examples/workers/basic-module/index.ts
 
 const worker = {
-  async fetch(request, _env, context) {
+  async fetch(request, env, context) {
+    enable('worker-example')
+
     const log = track(request, 'worker-example', reporter)
 
     log.info('gearing up to make a response')
